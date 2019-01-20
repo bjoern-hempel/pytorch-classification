a2c97a5c3c902e19bb66999b2b4a1d3cfdd2dc83
Björn Hempel <bjoern@hempel.li>
2019-01-20 18:05:17 +0100

    add exceptions, asserts and the first plotter

--- src/dimensionality-reduction-2d.py
+++ src/dimensionality-reduction-2d.py
@@ -25,10 +25,17 @@ __status__ = "Production"
 
 import pprint
 import math
+import sys
+import matplotlib.pyplot as plt
 
 
 # Configure the pretty printer
 pp = pprint.PrettyPrinter(indent=4)
+
+
+# PointCalculationError class
+class PointCalculationError(Exception):
+    pass
 
 
 def get_interbreeding_indexes(index_count, including_same = False, cross = False, deep = 0) -> 'list':
@@ -91,8 +98,13 @@ def solve_quadratic_equation(a, b, c, direction='right'):
     """
     s = b ** 2 - 4 * a * c
 
+    # Ignore very small negative numbers (to avoid the following negative root square error)
+    if -1e-10 < s < 0:
+        s = 0
+
+    # negative root square error
     if s < 0:
-        raise ValueError('Try to calculate the square root of 2 of negative number!')
+        raise ValueError('Try to calculate the square root of 2 of negative number ({})!'.format(s))
 
     if direction == 'right':
         return (-b + s**(1 / 2)) / (2 * a)
@@ -132,7 +144,7 @@ def get_point_from_lengths_y_first(xA, yA, xB, yB, l2, l3, direction='right'):
     return [xC, yC]
 
 
-def get_point_from_lengths(point_A, point_B, length_1, length_2, direction='right'):
+def get_point_from_lengths(point_A, point_B, length_2, length_3, direction='right'):
     """Calculate the point from 2 points and 2 lengths with given direction (right or left)."""
     xA = point_A[0]
     yA = point_A[1]
@@ -141,31 +153,32 @@ def get_point_from_lengths(point_A, point_B, length_1, length_2, direction='righ
 
     # the same point was given
     if xA == xB and yA == yB:
-        if length_1 == 0 and length_2 == 0:
+        if length_2 == 0 and length_3 == 0:
             return point_A
         else:
             raise ValueError('Two same points are given, but l2 and l3 are greater than 0!')
 
     # calculate the distance of P_A and P_B
-    l1 = calculate_distance(point_A, point_B)
+    length_1 = calculate_distance(point_A, point_B)
 
-    # it is not possible to calculate a point, because the length of l2 and l3 must be greater than l1
-    if l1 > length_1 + length_2:
-        raise ValueError('The length of l2 and l3 must be greater than l1!')
+    # One edge length must not be longer than the other two.
+    assert(length_1 <= length_2 + length_3), 'The length of l2 and l3 must be greater than l1!'
+    assert(length_2 <= length_1 + length_3), 'The length of l1 and l3 must be greater than l2!'
+    assert(length_3 <= length_1 + length_2), 'The length of l1 and l2 must be greater than l3!'
 
     dist_y = abs(yA - yB)
     dist_x = abs(xA - xB)
 
     # the point is not in the same y postion
     if dist_y > dist_x:
-        return get_point_from_lengths_x_first(xA, yA, xB, yB, length_1, length_2, direction)
+        return get_point_from_lengths_x_first(xA, yA, xB, yB, length_2, length_3, direction)
 
     # the point is not in the same x postion
     elif dist_x > dist_y:
-        return get_point_from_lengths_y_first(xA, yA, xB, yB, length_1, length_2, direction)
+        return get_point_from_lengths_y_first(xA, yA, xB, yB, length_2, length_3, direction)
 
     # same distance
-    return get_point_from_lengths_x_first(xA, yA, xB, yB, length_1, length_2, direction)
+    return get_point_from_lengths_x_first(xA, yA, xB, yB, length_2, length_3, direction)
 
 
 def get_points_from_lengths(point_A, point_B, length_1, length_2):
@@ -208,9 +221,20 @@ def get_point(previously_calculated_points, length_array, current_point, cancel_
     if len(data) < 2:
         raise ValueError('Unsupported number of data!')
 
-    # if we do have the number of two of given data elemens, we can choose one direction (left or right)
+    # if we do have the number of two of given data elemens, we can still choose one direction (left or right)
     if len(data) == 2:
-        return get_point_from_lengths(data[0]['point'], data[1]['point'], data[0]['length'], data[1]['length'], 'right')
+        try:
+            point = get_point_from_lengths(
+                data[0]['point'],
+                data[1]['point'],
+                data[0]['length'],
+                data[1]['length'],
+                'right'
+            )
+        except AssertionError as error:
+            raise PointCalculationError()
+
+        return point
 
     # len of data is higher than 2 -> we do have to compare the direction of calculated point
     if len(data) >= 3:
@@ -222,25 +246,34 @@ def get_point(previously_calculated_points, length_array, current_point, cancel_
         for i in range(len(indexes)):
             # first save base points (left and right); one of these both is the right one
             if len(points) == 0:
-                points = get_points_from_lengths(
+                try:
+                    points = get_points_from_lengths(
+                        data[indexes[i][0]]['point'],
+                        data[indexes[i][1]]['point'],
+                        data[indexes[i][0]]['length'],
+                        data[indexes[i][1]]['length']
+                    )
+                except AssertionError:
+                    continue
+                continue
+
+            try:
+                compare_points = get_points_from_lengths(
                     data[indexes[i][0]]['point'],
                     data[indexes[i][1]]['point'],
                     data[indexes[i][0]]['length'],
                     data[indexes[i][1]]['length']
                 )
+            except AssertionError:
                 continue
 
             distances = get_best_distances(
                 distances,
                 points,
-                get_points_from_lengths(
-                    data[indexes[i][0]]['point'],
-                    data[indexes[i][1]]['point'],
-                    data[indexes[i][0]]['length'],
-                    data[indexes[i][1]]['length']
-                )
+                compare_points
             )
 
+            # cancel, if the distance to compared point is lower than cancel_distance
             if cancel_distance is not None:
                 for j in range(2):
                     if distances[j] < cancel_distance:
@@ -281,11 +314,34 @@ def calculate_points_from_lengths(length_array):
             points_calculated[current_point] = [0, length_array[0][1]]
             continue
 
-        points_calculated[current_point] = get_point(points_calculated, length_array, current_point, 1e-10)
+        try:
+            point = get_point(points_calculated, length_array, current_point, 1e-10)
+        except PointCalculationError:
+            sys.exit('It was not possible to calculate the current point ({}).'.format(current_point))
+
+        points_calculated[current_point] = point
 
     return points_calculated
 
 
+def print_points(points, title = None):
+    var_names = ['x', 'y', 'z']
+
+    if title is not None:
+        print('{}:'.format(title))
+
+    for i in range(len(points)):
+        output = ''
+        for j in range(len(points[i])):
+            output += '\t' if output else ''
+
+            var_name = var_names[j] if len(var_names) >= len(points[i]) else 'x{}'.format(j)
+
+            output += '{:<2} = {:6.1f}'.format(var_name, points[i][j])
+        print(output)
+    print('')
+
+
 
 P = [
     [1, 1],
@@ -297,13 +353,22 @@ P = [
 ]
 
 P = [
-    [4, 6],
-    [1, 3],
-    [3, 2],
-    [1, 1],
-    [4, 3],
-    [6, 2]
+    [4,   6, 1, -10,  0],
+    [1,   3, 1,  10, 10],
+    [3,   2, 1, -10,  2],
+    [1,   1, 1, -10,  3],
+    [4,   3, 0, -10,  0],
+    [6,   2, 0, -10,  0],
+    [10, 10, 0,  10,  0]
 ]
+
+P = [
+    [1, 1],
+    [2, 2],
+    [3, 3]
+]
+
+print_points(P, 'Given points')
 
 # calculate the length array from given points above
 lengths = calculate_length_array(P)
@@ -312,6 +377,7 @@ lengths = calculate_length_array(P)
 P_calculated = calculate_points_from_lengths(lengths)
 
 # print the calculated points
-for i in range(len(P_calculated)):
-    if len(P_calculated[i]) >= 2:
-        print('x = ', '{:.1f}'.format(P_calculated[i][0]), '\ty = ', '{:.1f}'.format(P_calculated[i][1]))
+print_points(P_calculated, 'Calculated points')
+
+plt.scatter(*zip(*P_calculated))
+plt.show()
