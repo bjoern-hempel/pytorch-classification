0d13bc45910ff1e230727708580dbc34860abfc6
Björn Hempel <bjoern@hempel.li>
2019-01-30 23:52:57 +0100

    add new class_name width

--- src/__print.py
+++ src/__print.py
@@ -6,7 +6,7 @@ def get_columns():
     """Returns all columns and the width of them"""
 
     label_width = 5
-    class_name_width = 19
+    class_name_width = 23
 
     return {
         'arch': [13, '<13', '>13', None],
