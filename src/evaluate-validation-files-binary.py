cf255fc56ecab6a41c3d5253b6377ef84544b174
Björn Hempel <bjoern@hempel.li>
2019-02-02 23:34:12 +0100

    add binary evaluation script

---
+++ src/evaluate-validation-files-binary.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""
+Get the overall accuracy from the validation csv files from binary train.
+
+
+Usage:
+
+user$ src/evaluate-validation-files-binary.py
+
+TODO: furthermore
+- point 1
+- point 2
+"""
+
+__author__ = "Björn Hempel"
+__copyright__ = "Copyright 2019, An ixnode project"
+__credits__ = ["Björn Hempel"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Björn Hempel"
+__email__ = "bjoern@hempel.li"
+__status__ = "Production"
+
+
+import argparse
+import pprint
+import os
+import data_helper
+import print_helper
+import args_helper
+
+print(123)
